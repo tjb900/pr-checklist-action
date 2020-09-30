0a287a545f8b7b2e0e49de94f74ccebadc496357
Tim Burgess <timb@dug.com>
2020-09-30 15:46:06 +0800

    logging

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import {context, getOctokit} from '@actions/github'
 
 async function run(): Promise<void> {
   try {
+    core.info("got here 0")
+
     const token: string = core.getInput('github-token', {required: true})
     const github = getOctokit(token)
-
     const text: string = core.getInput('text', {required: true})
 
     core.info("got here 1")
